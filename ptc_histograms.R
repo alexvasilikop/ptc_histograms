0861f7eec6061f13071bd09a1dafdd4e28c50f22
alexvasilikop <alexvasilikop@gmail.com>
2024-11-25 15:35:10 +0000

    add max values for histogrms

--- ptc_histograms.R
+++ ptc_histograms.R
@@ -1,8 +1,7 @@
-library(dplyr)
 library(ggplot2)
 
 ############################################################################################
-plot_histogram_read_counts_per_target <- function (x=vector(), count=numeric(), id=character()) {
+plot_histogram_read_counts_per_target <- function (x=vector(), count=numeric(), id=character(), max_value=numeric()) {
     #function to plot histogram of per target coverage per sample
     #validation of input vector
     if (length(x)==0) {
@@ -10,11 +9,21 @@ plot_histogram_read_counts_per_target <- function (x=vector(), count=numeric(), 
     }
 
     #print histogram for vector
-    data_v <- data.frame( values = x)
+    suppressWarnings(data_v <- data.frame( values = as.numeric(x) ))
+    #remove NAs
+    data_v <- data_v[!is.na(data_v$values),, drop=FALSE]
+    #if values larger than 2500 replace them with 2500
+    data_v$values[data_v$values>as.numeric(max_value)] <- as.numeric(max_value) #to avoid extremely large values skewing the histogram
     output_file <- paste0(id, "_histogram_of_read_counts_per_target_", count, ".pdf")
-    pdf(output_file)
-    ggplot(data = data_v, aes(x=values)) + geom_histogram(binwidth = 100, fill = "skyblue", color = "black", alpha = 0.7) + labs(title = paste("Read counts per target, req. sample id:", id), x = "Coverage per target", y = "Frequency") + theme_minimal()
-    dev.off()
+    median_value=median(data_v$values)
+    #add median dashed line to histogram
+    g <- ggplot(data = data_v, aes(x=values)) + geom_histogram(binwidth=50, color="black", fill="lightsteelblue3") + 
+                                                geom_vline(aes(xintercept=median_value), color="red", linetype="dashed", linewidth=1.3) +
+                                                annotate("text", median_value, y=10, color="red", label=paste("Median:", round(median_value)), vjust = 3.0, hjust=1.1, size = 4) +
+                                                labs(title = paste0("Read counts per target, req. sample id: ", id), x = "Read counts per target", y = "Frequency") + 
+                                                theme_minimal()
+    #save plot to pdf file
+    ggsave(output_file, plot = g)
 
     # Check if the file is created and is non-zero size
     if (file.info(output_file)$size == 0) {
@@ -29,8 +38,9 @@ if (length(args) < 1) {
     stop("Please provide dir path to per target coverage metrics files")
 }
 #fetch path to files from cmd arguments
-files_path <- args[1]
-cat("Path to metrics files provided:", files_path)
+files_path <- args[1] # to fetch list of ptc metrics for each sample
+max_value <- args[2] # maximum value for histogram -> values larger than this value are set to the max specified
+cat("Path to metrics files provided:", files_path, "\n")
 
 #List files and read files in a list of data frames
 data.list <- list.files(files_path, pattern="*.metrics", full.names=T )
@@ -57,7 +67,7 @@ read.counts <- setNames(read.counts, req.sample.ids)
 c <- 1
 for (id in names(read.counts)) {
     print(paste("Plotting histogram for sample ID:", id))
-    plot_histogram_read_counts_per_target(read.counts[[id]], c, id)
+    plot_histogram_read_counts_per_target(read.counts[[id]], c, id, max_value)
     c <- c+1
 }
 
